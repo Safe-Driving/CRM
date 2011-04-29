fff057367fe790e5272dd48b8a9fbfcc8d3760e5
Romain <romain@crezeo.com>
2011-04-29 22:17:58 +0200

    Initial Commit CRM

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRM::Application.config.secret_token = '18c5e810471788d72408a8619da0610fcd1e4e84be80b46e0e767dc76533084785046687adbf18ec4d4d1b78eb58ccd7deace0314a4dd493613e45cbf6789ef7'
